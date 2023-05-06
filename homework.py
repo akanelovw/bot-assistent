43ac8df44c5ff3189495e7f5435f366c573192cc
Никита Болдырев <krul1wtd@yandex.ru>
2023-05-06 17:14:01 +0700

    update

--- homework.py
+++ homework.py
@@ -58,7 +58,6 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Получение ответа API."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     try:
         homework_statuses = requests.get(
             ENDPOINT,
@@ -67,50 +66,42 @@ def get_api_answer(timestamp):
         )
     except requests.exceptions.RequestException as error:
         logging.error(f'Ошибка при запросе к основному API: {error}')
-        send_message(bot, 'Ошибка при запросе к основному API')
         raise ConnectionError(f'Ошибка при запросе к основному API: {error}')
     if homework_statuses.status_code != 200:
         logging.error('HTTPResponse is not 200')
-        send_message(bot, 'HTTPResponse is not 200')
         raise requests.exceptions.HTTPError('HTTPResponse is not 200')
-    if not isinstance(homework_statuses.json(), dict):
-        logging.error('Тип данных не является словарём')
-        send_message(bot, 'Тип данных не является словарём')
-        raise TypeError('response is not a dict')
-    return homework_statuses.json()
+    try:
+        return homework_statuses.json()
+    except TypeError:
+        logging.error('Данные не в json формате')
+        raise TypeError('Данные не в json формате')
 
 
 def check_response(response):
     """Проверка типа данных."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     if not isinstance(response, dict):
         logging.error('Тип данных не является словарём')
-        ResponseType = type(response)
-        raise TypeError(f'response is {ResponseType}, expected a dict')
+        response_type = type(response)
+        raise TypeError(f'response is {response_type}, expected a dict')
     if 'homeworks' not in response:
         logging.error('Ключ homeworks отсутствует в словаре')
-        send_message(bot, 'Ключ homeworks отсутствует в словаре')
         raise KeyError('Ключа homeworks нет в словаре')
     if not isinstance(response['homeworks'], list):
         logging.error('Тип данных не является списком')
-        ResponseType = type(response['homeworks'])
-        # send_message(bot, 'Тип данных не является списком')
-        raise TypeError(f'response is {ResponseType}, expected a list')
+        response_type = type(response['homeworks'])
+        raise TypeError(f'response is {response_type}, expected a list')
     return response
 
 
 def parse_status(homework):
     """Проверка статуса работы."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     homework_name = homework.get('homework_name')
     status = homework.get('status')
     if homework_name is None:
         logging.error('Нет ключа homework_name в словаре')
-        send_message(bot, 'Нет ключа homework_name в словаре')
         raise KeyError('Нет ключа в словаре')
     if status not in HOMEWORK_VERDICTS:
         logging.error('Статус работы неизвестен')
-        send_message(bot, 'Статус работы неизвестен')
         raise ValueError('Status is None')
     verdict = HOMEWORK_VERDICTS[status]
     return (f'Изменился статус проверки работы "{homework_name}".'
@@ -126,16 +117,20 @@ def main():
     while True:
         try:
             response = check_response(get_api_answer(timestamp))
-            if response:
+            if response['homeworks']:
                 message = parse_status(response['homeworks'][0])
-            if message != last_status:
-                send_message(bot, message)
-                last_status = message
-                logging.debug('Статус изменился')
+                if message != last_status:
+                    send_message(bot, message)
+                    last_status = message
+                    logging.debug('Статус изменился')
         except requests.exceptions.RequestException:
             logging.error('Ошибка при запросе к основному API')
-            send_message(bot, 'Ошибка при запросе к основному API')
-            raise ConnectionError('Ошибка при запросе к основному API')
+            try:
+                send_message(bot, 'Ошибка при запросе к основному API')
+            except telegram.error.TelegramError:
+                logging.error('Ошибка при отправке сообщения')
+                raise telegram.error.TelegramError(
+                    'Не удалось отправить сообщение')
         finally:
             time.sleep(RETRY_PERIOD)
 
